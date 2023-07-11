1a5530388861bdd1d2d0a2892bd12b3278ac4ebf
Collin Bolles <collinbolles@gmail.com>
2023-07-11 10:37:19 -0400

    chore: Project Setup #1
    * Minimum skeleton to run JupyterHub with DockerSpawner
    * Placeholder for custom spawner
    * Placeholder for custom authenticator
    * Dockerfile for deployment
    * Instructions for running

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Start with JupyterHub base
+ARG JUPYTERHUB_VERSION=4.0.1
+FROM jupyterhub/jupyterhub:$JUPYTERHUB_VERSION
+
+# Install custom spawner
+COPY filespawner /tmp/filespawner
+RUN cd /tmp/filespawner && python3 -m pip install .
+
+# Install custom authenticator
+COPY jwtauth /tmp/jwtauth
+RUN cd /tmp/jwtauth && python3 -m pip install .
+
+# Include jupyter hub config
+COPY jupyterhub_config.py /srv/jupyterhub_config.py
+
+CMD ["jupyterhub", "-f", "/srv/jupyterhub_config.py"]
---
+++ jupyterhub_config.py
@@ -0,0 +1,27 @@
+c = get_config()
+
+## Network Settings
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = '0.0.0.0'
+# the hostname/ip that should be used to connect to the hub
+# this is usually the hub container's name
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+## Authenticator Setting
+# Use custom authenticator
+# c.JupyterHub.authenticator_class = 'jwtauth.JwtAuth'
+c.JupyterHub.authenticator_class = 'dummy'
+
+## Spawner Settings
+# Use custom spawner
+# c.JupyterHub.spawner_class = 'filespawner.FileSpawner'
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# Default image for user server containers
+c.DockerSpawner.image = 'jupyter/datascience-notebook'
+
+# Network used to communicate with user containers
+c.DockerSpawner.network_name = 'jupyterhub'
+
+# Delete containers when they are stopped
+c.DockerSpawner.remove = True
