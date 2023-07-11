5727cdec19757c02a933873bfefe545e892d9792
cbolles <collinbolles@gmail.com>
2023-07-11 11:52:03 -0400

    Working ability to download file into container

--- filespawner/filespawner/filespawner.py
+++ filespawner/filespawner/filespawner.py
@@ -1,16 +1,41 @@
 from dockerspawner import DockerSpawner
+import requests
+from tornado import web
+import docker
+import os
+import tarfile
+
+
+async def copy_to(docker, src, container_id):
+    srcname = os.path.basename(src)
+    tar = tarfile.open(src + '.tar', mode='w')
+    try:
+        tar.add(srcname)
+    finally:
+        tar.close()
+
+    data = open(src + '.tar', 'rb').read()
+    await docker('put_archive', container=container_id, path='/home/jovyan', data=data)
+
+
 
 class FileSpawner(DockerSpawner):
-    def options_from_query(self, query_data):
-        print('query data')
-        print(query_data)
-        return super().options_from_query(query_data)
-
     async def start(self):
-        print('Form Data')
-        print(self.user_options)
+        # Get the URL of the file to download
+        file_url = self.user_options.get('fileURL', None)
+        if file_url is None:
+            raise web.HTTPError(400, 'Missing fileURL')
 
-        print('cmd')
-        print(self.cmd)
+        # Download and save the file
+        request = requests.get(file_url)
+        open('file.ipynb', 'wb').write(request.content)
 
-        return await super().start()
+        # Pass the file to the notebook
+
+        # Start the docker notebook
+        result = await super().start()
+
+        # Pass the file to the container
+        await copy_to(self.docker, 'file.ipynb', self.container_id)
+
+        return result
