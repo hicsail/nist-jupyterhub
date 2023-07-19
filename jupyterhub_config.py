f87983d4f7eaf5fe385ac218403a04492183dd86
cbolles <collinbolles@gmail.com>
2023-07-19 10:06:07 -0400

    Add service account

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -16,10 +16,37 @@ c.JupyterHub.authenticator_class = 'null'
 c.JupyterHub.spawner_class = 'filespawner.FileSpawner'
 
 # Default image for user server containers
-c.DockerSpawner.image = 'jupyter/datascience-notebook'
+c.DockerSpawner.image = 'jupyter/base-notebook'
 
 # Network used to communicate with user containers
 c.DockerSpawner.network_name = 'jupyterhub'
 
 # Delete containers when they are stopped
 c.DockerSpawner.remove = True
+
+## API Settings
+# Create the NIST service
+c.JupyterHub.services = [
+    { 'name': 'service-NIST', 'api_token': '327b5d481483f480c2978cc49922a00506b2bf7ae707bb1e8703ad082f0fc0ca' }
+]
+
+# Grant NIST service account admin privledges
+c.JupyterHub.load_roles = [
+    {
+        'name': 'service-role',
+        'scopes': [
+            # Ability to control users
+            'admin:users',
+            # Ability to start/stop/delete servers
+            'admin:servers',
+            # Ability to make tokens
+            'tokens',
+            # See current users
+            'list:users'
+        ],
+        'services': [
+            # assign the service the above permissions
+            'service-NIST'
+        ],
+    }
+]
